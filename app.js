86c050c5bbb035a78ffeae71340919ca99ad95a3
rakshika <rakshikaraju@gmail.com>
2022-11-19 18:12:15 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// const express = require('express')
+import express from 'express';
+import cors from 'cors'
+import HelloController from "./controllers/hello-controller.js";
+import UsersController from "./controllers/users/users-controller.js";
+import UserController from "./controllers/users/users-controller.js";
+import TuitsController from "./controllers/tuits/tuits-controller.js"
+const app = express()
+app.use(cors())
+app.use(express.json());
+
+HelloController(app)
+UserController(app)
+TuitsController(app)
+
+app.listen(process.env.PORT || 4000);
+
+
+
+// app.get('/hello', () => {console.log("Hello World!")})
+// app.get('/hello', (req, res) => {res.send("Hello World!")})
+// app.get('/hello', (req, res) => {res.send('Life is good!')})
+// app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
---
+++ controllers/tuits/tuits-controller.js
@@ -0,0 +1,37 @@
+import posts from "./tuits.js";
+let tuits = posts;
+
+const findTuits = (req, res) => {
+    res.json(tuits);
+}
+const createTuit = (req, res) => {
+    const tuitContent = req.body;
+    tuitContent._id = (new Date()).getTime() + ' ';
+    tuitContent.likes = 0;
+    tuitContent.liked = false;
+    tuits.push(tuitContent);
+    res.json(tuitContent);
+}
+// in the json file, had to make id a string to get these working
+// or dont use !== or === since != or ==
+const updateTuit = (req, res) => {
+    const tuitIdToUpdate = req.params.tid;
+    const updates = req.body;
+    const tuitIndex = tuits.findIndex(
+        (t) => t._id == tuitIdToUpdate)
+        tuits[tuitIndex] = {...tuits[tuitIndex], ...updates};
+    res.sendStatus(200);
+}
+const deleteTuit = (req, res) => {
+    const tuitdIdToDelete = req.params.tid;
+    tuits = tuits.filter((t) =>
+                             t._id != tuitdIdToDelete);
+    res.sendStatus(200);
+}
+
+export default (app) => {
+    app.get('/api/tuits', findTuits);
+    app.post('/api/tuits', createTuit);
+    app.put('/api/tuits/:tid', updateTuit);
+    app.delete('/api/tuits/:tid', deleteTuit);
+}
